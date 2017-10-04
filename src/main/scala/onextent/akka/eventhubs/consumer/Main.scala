18d23e9dce17fdb97e59c1a0e42316b2bd35f94b
Ed Sweeney <esweeney@starbucks.com>
2017-10-04 16:54:49 -0700

    np

--- src/main/scala/onextent/akka/eventhubs/consumer/Main.scala
+++ src/main/scala/onextent/akka/eventhubs/consumer/Main.scala
@@ -1,19 +1,17 @@
 package onextent.akka.eventhubs.consumer
 
 import akka.Done
-import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
-import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
-import com.microsoft.azure.reactiveeventhubs.{EventHubsMessage, SourceOptions}
-import com.microsoft.azure.reactiveeventhubs.scaladsl._
 import com.microsoft.azure.reactiveeventhubs.ResumeOnError._
+import com.microsoft.azure.reactiveeventhubs.scaladsl._
+import com.microsoft.azure.reactiveeventhubs.{EventHubsMessage, SourceOptions}
 import com.typesafe.scalalogging.LazyLogging
 import onextent.akka.eventhubs.consumer.models.JsonSupport
 import onextent.akka.eventhubs.consumer.routes.CacherRoute
 
-import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.concurrent.Future
 
 object Main extends App with LazyLogging with JsonSupport with ErrorSupport {
 
@@ -34,10 +32,6 @@ object Main extends App with LazyLogging with JsonSupport with ErrorSupport {
     .alsoTo(console2)
     .to(console)
     .run()
-
-  // implicit val system: ActorSystem = ActorSystem("web-cache-system")
-  // implicit val materializer: ActorMaterializer = ActorMaterializer()
-  // implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   val route =
     HealthCheck ~
