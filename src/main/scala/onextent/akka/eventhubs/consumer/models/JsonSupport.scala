49f81d8f7f6f853d06a6727a60817d9730ae2307
Ed Sweeney <ed@onextent.com>
2017-10-06 08:16:41 -0700

    fix compile error from refactoring Assessment

--- src/main/scala/onextent/akka/eventhubs/consumer/models/JsonSupport.scala
+++ src/main/scala/onextent/akka/eventhubs/consumer/models/JsonSupport.scala
@@ -3,7 +3,6 @@ package onextent.akka.eventhubs.consumer.models
 import java.util.{Date, UUID}
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import onextent.akka.eventhubs.consumer.AssessmentCacher.Assessment
 import spray.json._
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
